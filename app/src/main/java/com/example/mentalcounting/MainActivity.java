76c13b1253f383f04eba090e3b63cf10729388cb
Nitcheu <ph.favrelle@gmail.com>
2022-02-02 13:47:34 +0100

    Version graphique

--- app/src/main/java/com/example/mentalcounting/MainActivity.java
+++ app/src/main/java/com/example/mentalcounting/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.mentalcounting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Associer une action au bouton play
+
+        associateOpenActivityToButton(R.id.play_button, PlayActivity.class);
+
+        // Associer une action au bouton resultat
+
+        associateOpenActivityToButton(R.id.res_button, ResActivity.class);
+
     }
+
+    private void associateOpenActivityToButton(int id, Class activity){
+        Button button = findViewById(id);
+        button.setOnClickListener(view -> openActivity(activity));
+    }
+
+    private void openActivity(Class activity){
+        Intent intent = new Intent(this, activity);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/mentalcounting/ResActivity.java
+++ app/src/main/java/com/example/mentalcounting/ResActivity.java
@@ -3,6 +3,7 @@ package com.example.mentalcounting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ResActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class ResActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_res);
+
+        // Permet de retourner sur l'activité précédente
+        Button button = findViewById(R.id.previous_button);
+        button.setOnClickListener(view -> finish());
+
     }
+
+
+
 }
